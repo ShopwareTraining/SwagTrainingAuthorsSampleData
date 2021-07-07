9425e3e98b198b548a0f42e6709352f3ed8c0fd1
jisse Reitsma <jisse@yireo.com>
2021-07-07 16:59:23 +0200

    Adding all files to git via Yireo Command

---
+++ src/Migration/Migration1610022539.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace SwagTraining\AuthorsSampleData\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+class Migration1610022539 extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1610022539;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sampleData = $this->getSampleData();
+        foreach ($sampleData as $sampleDataEntry) {
+            $sampleDataEntry['id'] = Uuid::randomBytes();
+            $sampleDataEntry['created_at'] = date('Y:m:d H:i:s', time());
+            $connection->insert('author', $sampleDataEntry);
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+
+    /**
+     * @return array
+     */
+    private function getSampleData(): array
+    {
+        return [
+            [
+                'name' => 'Rico Neitzel',
+                'description' => 'Shopware trainer',
+                'birthdate' => '1970-01-01',
+            ],
+            [
+                'name' => 'Jisse Reitsma',
+                'description' => 'Shopware trainer',
+                'birthdate' => '1978-07-02'
+            ]
+        ];
+    }
+}
---
+++ src/SwagTrainingAuthorsSampleData.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace SwagTraining\AuthorsSampleData;
+
+use Shopware\Core\Framework\Plugin;
+
+class SwagTrainingAuthorsSampleData extends Plugin
+{
+}
